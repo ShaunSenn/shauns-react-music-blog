62a38df5ea98c26d09d3f27db4a02dca69dc3c60
Senn <shaunsenn2019@gmail.com>
2022-06-08 14:10:28 -0400

    spelling error fixed for final submission

--- src/components/parent_pages/home.js
+++ src/components/parent_pages/home.js
@@ -18,7 +18,7 @@ export default function Home() {
                 </div>
                 <div className='right-column'>
                     <div className='square-container'>
-                        <p>Enjoy a short clip of my top two song of the week below!</p>
+                        <p>Enjoy a short clip of my top song of the week below!</p>
                     </div>
                     <div className='square-container'>
                         <iframe title="Title Song 1" src="https://open.spotify.com/embed/track/2jzeP1ysUBZzpNS4DW9DGK?utm_source=generator&theme=0" width="200px" height="80" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
